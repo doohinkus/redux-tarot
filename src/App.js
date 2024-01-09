15d8b624d9d62504f487d0ac92c7bd5017ce99a0
Valen Longfeather <valenlongfeather@Valens-MacBook-Pro.local>
2024-01-09 14:55:01 -0800

    formating

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 /* eslint-disable react/forbid-prop-types */
 /* eslint-disable react/jsx-filename-extension */
-
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
@@ -8,9 +7,10 @@ import { flipCard, getThreeCards, dadJoke } from './actions';
 import './App.css';
 import Card from './components/Card';
 // pull the state you want to use
-const mapStateToProps = state => ({
-  selectedCards: state.selectedCards,
+const mapStateToProps = (state) => ({
+  selectedCards: state.selectedCards
 });
+
 // pull the actions you want to use
 const mapDispatchToProps = {
   flipCard,
@@ -24,9 +24,8 @@ function App(props) {
     <div className="App">
       <section className="App-header">
         <h1>Marseille Tarot</h1>
-        <button type="button" onClick={() => props.dadJoke()}>joke</button>
         <button
-        // no need to destructure actions
+          // no need to destructure actions
           onClick={() => props.getThreeCards()}
           type="button"
           className="no-border"
@@ -40,18 +39,19 @@ function App(props) {
         </button>
         <span className="small">shuffle</span>
         <div className="grid">
-          {selectedCards && selectedCards.map(card => (
-            <React.Fragment key={card.card}>
-              <Card
-                title={card.title}
-                faceValue={card.img}
-                isFlipped={card.flipped}
-                tagline={card.tagline}
-                meaning={card.article}
-                flipCard={() => props.flipCard(card.card)}
-              />
-            </React.Fragment>
-          ))}
+          {selectedCards &&
+            selectedCards.map((card) => (
+              <React.Fragment key={card.card}>
+                <Card
+                  title={card.title}
+                  faceValue={card.img}
+                  isFlipped={card.flipped}
+                  tagline={card.tagline}
+                  meaning={card.article}
+                  flipCard={() => props.flipCard(card.card)}
+                />
+              </React.Fragment>
+            ))}
         </div>
       </section>
     </div>
@@ -60,8 +60,7 @@ function App(props) {
 App.propTypes = {
   selectedCards: PropTypes.array.isRequired,
   flipCard: PropTypes.func.isRequired,
-  dadJoke: PropTypes.func.isRequired,
-  getThreeCards: PropTypes.func.isRequired,
+  getThreeCards: PropTypes.func.isRequired
 };
 // glue redux to this component
 export default connect(mapStateToProps, mapDispatchToProps)(App);
