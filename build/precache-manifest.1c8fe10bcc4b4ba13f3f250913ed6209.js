81bf442d79424299fe6da00778cd14ebbd382309
Valen Longfeather <valenlongfeather@Valens-MacBook-Pro.local>
2024-01-09 14:41:42 -0800

    remove build from ignore

---
+++ build/precache-manifest.1c8fe10bcc4b4ba13f3f250913ed6209.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57e60af5dcb7ece2982e04b683350773",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54d4d3cccaee5b1838c4",
+    "url": "/static/css/main.3b028880.chunk.css"
+  },
+  {
+    "revision": "bf31d48eb3a4b651c161",
+    "url": "/static/js/2.58f9b0b0.chunk.js"
+  },
+  {
+    "revision": "54d4d3cccaee5b1838c4",
+    "url": "/static/js/main.e6fec352.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
